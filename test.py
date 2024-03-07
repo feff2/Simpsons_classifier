463025b6e66a60a19958bb035e0d84d9981da5c0
feff2 <145095861+feff2@users.noreply.github.com>
2024-03-08 03:32:30 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -31,26 +31,4 @@ def make_f1_score(model,dataset):
 
     f1_micro = round(f1_score(actual_labels, preds_class, average='micro'), 3)
 
-    num_classes = len(list(set(actual_labels)))
-    fpr = {}
-    tpr = {}
-    roc_auc = {}
-    for i in range(num_classes):  
-        fpr[i], tpr[i], _ = roc_curve(actual_labels, probs_ims[:, i], pos_label=i)
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    plt.figure()
-    lw = 2
-    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-    for i, color in zip(range(num_classes), colors):
-        plt.plot(fpr[i], tpr[i], color=color, lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[i])
-    plt.plot([0, 1], [0, 1], color='gray', lw=lw, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic')
-    plt.legend(loc="lower right")
-    plt.show()
-
     return f1_micro
